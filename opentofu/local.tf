46d805dae9e3a2f2383f5d8406fdceacb5d2b7a3
Codesenju <codesenju@gmail.com>
2025-03-28 11:52:22 +0200

    fix prowler and increase k8s resources

--- opentofu/local.tf
+++ opentofu/local.tf
@@ -4,8 +4,8 @@ locals {
   vm_password = "ubuntu"
   net = "192.168.0" # First 3 octets of your home network, Change me if you want
 
-  worker_cores  = 4
-  worker_memory = 6144
+  worker_cores  = 6
+  worker_memory = 8192
   control_plane_cores  = 4
   control_plane_memory = 6144
   worker_disk_size = 50
