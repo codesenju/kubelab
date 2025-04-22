683f7db49358741c770ed9779d7505aff741520e
Codesenju <codesenju@gmail.com>
2025-04-22 12:52:38 +0200

    added headscale

--- CloudStack/tofu/main.tf
+++ CloudStack/tofu/main.tf
@@ -6,12 +6,12 @@ resource "proxmox_virtual_environment_vm" "mng1" {
   vm_id       = 10001
 
   cpu {
-    cores = 4
+    cores = local.mng_cpu
     type = "host"
   }
 
   memory {
-    dedicated = 8192
+    dedicated = local.mng_memory
   }
 
   disk {
@@ -57,63 +57,63 @@ resource "proxmox_virtual_environment_vm" "mng1" {
   }
 }
 
-resource "proxmox_virtual_environment_vm" "kvm_host_1" {
-  name        = "kvm-host-1"
-  node_name   = "kubelab-2"
-  description = "KVM host 1"
-  vm_id       = 10002
+# resource "proxmox_virtual_environment_vm" "kvm1" {
+#   name        = "kvm1"
+#   node_name   = "kubelab-2"
+#   description = "KVM host 1"
+#   vm_id       = 10002
 
-  cpu {
-    cores = 4
-    type = "host"
-  }
+#   cpu {
+#     cores = local.kvm_cpu
+#     type = "host"
+#   }
 
-  memory {
-    dedicated = 8192
-  }
+#   memory {
+#     dedicated = local.kvm_memory
+#   }
 
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = "local:iso/noble-server-cloudimg-amd64.img"
-    interface    = "virtio0"
-    iothread     = true
-    discard      = "on"
-    size         = 300
-  }
+#   disk {
+#     datastore_id = "local-lvm"
+#     file_id      = "local:iso/noble-server-cloudimg-amd64.img"
+#     interface    = "virtio0"
+#     iothread     = true
+#     discard      = "on"
+#     size         = 300
+#   }
 
-  network_device {
-    bridge = "vmbr0"
-    model  = "virtio"
-    # vlan_id = XXX
-  }
+#   network_device {
+#     bridge = "vmbr0"
+#     model  = "virtio"
+#     # vlan_id = XXX
+#   }
 
-  operating_system {
-    type = "l26" # Linux kernel type
-  }
+#   operating_system {
+#     type = "l26" # Linux kernel type
+#   }
 
-  initialization {
-    ip_config {
-      ipv4 {
-        address = "192.168.0.100/24"
-        gateway = "192.168.0.1"
-      }
-    }
+#   initialization {
+#     ip_config {
+#       ipv4 {
+#         address = "192.168.0.100/24"
+#         gateway = "192.168.0.1"
+#       }
+#     }
 
-    dns {
-      servers = ["192.168.0.15","1.1.1.1"] # DNS servers
-    }
+#     dns {
+#       servers = ["192.168.0.15","1.1.1.1"] # DNS servers
+#     }
 
-    user_account {
-      username = "cloud"
-      password = "cloud"
-      keys     = [file("../../kubelab.pub")]
-    }
-  }
-  # create lifecycle to ignore changes to keys
-  lifecycle {
-    ignore_changes = [initialization[0].user_account[0].keys]
-  }
-}
+#     user_account {
+#       username = "cloud"
+#       password = "cloud"
+#       keys     = [file("../../kubelab.pub")]
+#     }
+#   }
+#   # create lifecycle to ignore changes to keys
+#   lifecycle {
+#     ignore_changes = [initialization[0].user_account[0].keys]
+#   }
+# }
 
 # resource "proxmox_virtual_environment_download_file" "kubelab" {
 #   count       = 2
