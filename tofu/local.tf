f1831be2d59c4a68c277ec22c753a5b92bde77f0
Jazziro <codesenju@gmail.com>
2025-07-24 07:52:14 +0200

    updated mimir replicas

--- tofu/local.tf
+++ tofu/local.tf
@@ -3,8 +3,8 @@ locals {
   vm_user = "ubuntu"
   vm_password = "ubuntu"
   net = "192.168.0" # First 3 octets of your home network, Change me if you want
-  worker_cores  = 6
-  worker_memory = 10240
+  worker_cores  = 8
+  worker_memory = 12288
   control_plane_cores  = 2
   control_plane_memory = 4096
   worker_disk_size = 60
