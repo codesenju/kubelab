847da76913892ea4f4f769426a9bf8ca6db6f112
codesenju <codesenju@gmail.com>
2025-11-02 16:08:03 +0200

    Update worker VM resources and fix node distribution
    - Comment out file_id to prevent VM replacement
    - Update worker CPU cores to 12 and memory to 16384MB to match actual VMs
    - Add comment explaining conditional node assignment logic
    - Successfully applied disk expansion to 80GB for all workers

--- tofu/local.tf
+++ tofu/local.tf
@@ -1,12 +1,12 @@
 locals {
-  public_key_file_path = "./../../kubelab.pub" # Change me
+  public_key_file_path = "~/codesenju/kubelab.pub" # Change me
   vm_user = "ubuntu"
   vm_password = "ubuntu"
   net = "192.168.0" # First 3 octets of your home network, Change me if you want
-  worker_cores  = 8
-  worker_memory = 12288
+  worker_cores  = 12
+  worker_memory = 16384
   control_plane_cores  = 2
   control_plane_memory = 4096
-  worker_disk_size = 60
+  worker_disk_size = 80
   control_plane_disk_size = 30
 }
